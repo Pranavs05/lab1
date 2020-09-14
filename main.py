ade690c1cd63d4275f1423364a84dec4117c5ee7
Pranav Shirke <pranavshirke@Pranavs-MacBook-Pro.local>
2020-09-14 13:58:51 -0400

    Update

---
+++ main.py
@@ -0,0 +1,17 @@
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    
+    app.run(host='127.0.0.1', port=8080, debug=True)
+
